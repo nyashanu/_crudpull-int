8a3da30d5aad83095de524781deec3aa3a89ad90
nyashanu <lankienyashanu@gmail.com>
2017-09-25 03:44:51 +0200

    jk

--- index.js
+++ index.js
@@ -1,9 +1,85 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://test:test@ds015878.mlab.com:15878/heroku_q0q4cd13');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// View Engine
+// app.set('views', path.join(__dirname, 'views'));
+// app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+// app.set('view engine', 'handlebars');
 
-app.use(express.static(__dirname + '/public'));
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -13,13 +89,17 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-// app.get('/private_beta', function(request, response) {
-//   response.render('pages/private_beta');
-// });
+app.get('/private_beta', function(request, response) {
+  response.render('pages/private_beta');
+});
 
 // app.get('/login', function(request, response) {
 //   response.render('pages/login');
 // });
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
